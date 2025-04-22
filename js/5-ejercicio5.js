e640cfa591813a14bec6834eaf89d2b863182b3e
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-04-22 20:44:11 -0300

    Realicé el ejercicio 7 del tp de bucles

--- js/5-ejercicio5.js
+++ js/5-ejercicio5.js
@@ -14,56 +14,56 @@ Output: ‘L’
 
 const dni = parseInt(prompt("Ingresa tu DNI: "));
 
-if(dni >= 0 && dni <= 99999999){
+if (dni >= 0 && dni <= 99999999) {
+
     const resultado = dni % 23;
     document.writeln("Tu número de DNI se corresponde con la letra: ");
-    if(resultado === 0){
-        document.writeln("T");
-    }else if(resultado === 1){
-        document.writeln("R");
-    }else if(resultado === 2){
-        document.writeln("W");
-    }else if(resultado === 3){
-        document.writeln("A");
-    }else if(resultado === 4){
-        document.writeln("G");
-    }else if(resultado === 5){
-        document.writeln("M");
-    }else if(resultado === 6){
-        document.writeln("Y");
-    }else if(resultado === 7){
-        document.writeln("F");
-    }else if(resultado === 8){
-        document.writeln("P");
-    }else if(resultado === 9){
-        document.writeln("D");
-    }else if(resultado === 10){
-        document.writeln("X");
-    }else if(resultado === 11){
-        document.writeln("B");
-    }else if(resultado === 12){
-        document.writeln("N");
-    }else if(resultado === 13){
-        document.writeln("J");
-    }else if(resultado === 14){
-        document.writeln("Z");
-    }else if(resultado === 15){
-        document.writeln("S");
-    }else if(resultado === 16){
-        document.writeln("Q");
-    }else if(resultado === 17){
-        document.writeln("V");
-    }else if(resultado === 18){
-        document.writeln("H");
-    }else if(resultado === 19){
-        document.writeln("L");
-    }else if(resultado === 20){
-        document.writeln("C");
-    }else if(resultado === 21){
-        document.writeln("K");
-    }else if(resultado === 22){
-        document.writeln("E");
-    }    
-}
 
-// (T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E) 
+    if (resultado === 0) {
+        document.writeln("T");
+    } else if (resultado === 1) {
+        document.writeln("R");
+    } else if (resultado === 2) {
+        document.writeln("W");
+    } else if (resultado === 3) {
+        document.writeln("A");
+    } else if (resultado === 4) {
+        document.writeln("G");
+    } else if (resultado === 5) {
+        document.writeln("M");
+    } else if (resultado === 6) {
+        document.writeln("Y");
+    } else if (resultado === 7) {
+        document.writeln("F");
+    } else if (resultado === 8) {
+        document.writeln("P");
+    } else if (resultado === 9) {
+        document.writeln("D");
+    } else if (resultado === 10) {
+        document.writeln("X");
+    } else if (resultado === 11) {
+        document.writeln("B");
+    } else if (resultado === 12) {
+        document.writeln("N");
+    } else if (resultado === 13) {
+        document.writeln("J");
+    } else if (resultado === 14) {
+        document.writeln("Z");
+    } else if (resultado === 15) {
+        document.writeln("S");
+    } else if (resultado === 16) {
+        document.writeln("Q");
+    } else if (resultado === 17) {
+        document.writeln("V");
+    } else if (resultado === 18) {
+        document.writeln("H");
+    } else if (resultado === 19) {
+        document.writeln("L");
+    } else if (resultado === 20) {
+        document.writeln("C");
+    } else if (resultado === 21) {
+        document.writeln("K");
+    } else if (resultado === 22) {
+        document.writeln("E");
+    }
+}
---
+++ js/7-ejercicio7.js
@@ -0,0 +1,22 @@
+/* 7- Haz un script que escriba una pirámide inversa de los números del 1 al número que indique el usuario (no mayor de 50)  de la siguiente forma : (suponiendo que indica 30).
+
+303030303030303030303030303030303030303030303030303030303030
+2929292929292929292929292929292929292929292929292929292929
+28282828282828282828282828282828282828282828282828282828
+…..
+333
+22
+1 */
+
+for(let i = 5; i > 0; i--){
+    document.writeln(`<b>${i}</b>`);
+    for(let j = i; j > 1; j--){
+        if(i <= 9){
+            document.writeln(`&nbsp;&nbsp;<b>${i}</b>`);    
+        }
+        else{
+            document.writeln(`<b>${i}</b>`);
+        }
+    }
+    document.writeln("<br>");
+}
